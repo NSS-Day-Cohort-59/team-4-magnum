cc40c8cc13f2cb857a25ae0bcbbb36f15b8a6f0a
Sarah Beth Langley <sarahbethdoescode@gmail.com>
2022-09-22 11:07:05 -0500

    populated actor names in database

--- scripts/database.js
+++ scripts/database.js
@@ -1,4 +1,27 @@
 const database = {
+  actors: [{
+    id: 1,
+    name: "Ashton Kutcher"
+  }, {
+    id: 2,
+    name: "Jodie Foster"
+  }, {
+    id: 3,
+    name: "Denzel Washington"
+  }, {
+    id: 4,
+    name: "Whoopi Goldberg"
+  }, {
+    id: 5,
+    name: "Jasmine Somebody"
+  }, {
+    id: 6,
+    name: "Alladin Dude"
+  }, {
+    id: 7,
+    name: "Jaffar Badguy"
+  }]
+
 
 }
 
